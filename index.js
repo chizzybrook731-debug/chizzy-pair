a91e7bee8adf35803e6f297b5332f3c962cccaa0
chizzybrook731-debug <chizzybrook731@gmail.com>
2026-07-16 13:52:32 +0200

    add baileys pairing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { default: makeWASocket, useMultiFileAuthState, delay } = require('@whiskeysockets/baileys');
 const app = express();
 
 app.use(express.json());
@@ -9,8 +10,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/pair', (req, res) => {
-  res.json({ code: "TEMP-CODE-123" });
+app.post('/pair', async (req, res) => {
+  const { number } = req.body;
+  const { state, saveCreds } = await useMultiFileAuthState('./auth');
+  
+  const sock = makeWASocket({
+    auth: state,
+    printQRInTerminal: false
+  });
+  
+  sock.ev.on('creds.update', saveCreds);
+  
+  await delay(2000);
+  const code = await sock.requestPairingCode(number);
+  
+  res.json({ code: code });
 });
 
 module.exports = app;
